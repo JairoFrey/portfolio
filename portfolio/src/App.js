3ae6a4a7bd3f0a345e16141acb92c7cf94965e64
JairoFrey <jayro15@yahoo.com>
2018-05-26 15:38:30 -0500

    heroku commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hero from "./Components/Hero"
+import HImage from "./Pictures/heroimage.jpg"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <Hero backgroundImage={HImage}></Hero>
+        </div>
+        <div>This is a test</div>
       </div>
     );
   }
